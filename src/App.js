9ee982218f1b859db67d253a5f02dbda640425fe
polyester_ctrl <shunfreed2016@gmail.com>
2020-10-10 15:54:53 +0900

    APIに取得しに行く

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Top from './screens/Top.js';
+import NavBar from './components/NavBar.js';
+import Quiz from './screens/Quiz.js';
+
 
 function App() {
   return (
-    <div className="App">
-      aaaa
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
